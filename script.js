9ed6220240e8cff79098e814fd0768aacb4d58cd
Dgliump <lortkiphanidzeliaa@gmail.com>
2023-09-30 02:20:13 -0400

    finish

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const boldArray = new Array(bold.length);
 const imgArray = new Array(img.length);
 const answerArray = new Array(answer.length);
 
-
 let before;
 let boldFont;
 let rotateImg;
@@ -16,26 +15,14 @@ for (let i = 0; i < bold.length; i++) {
         if (before) {
             before.style.display = "none";
             boldFont.style.fontWeight = "400";
-            rotateImg.style.transform = "rotate(180deg)";
-            question.style.display.justifyContent = "space between";
+            rotateImg.style.transform = "rotate(360deg)";
         }
+
         bold[i].parentElement.nextElementSibling.style.display = "block";
         before = bold[i].parentElement.nextElementSibling;
         bold[i].style.fontWeight = "900";
         boldFont = bold[i];
         img[i].style.transform = "rotate(180deg)";
-        question.style.justifyContent = "space between";
         rotateImg = img[i];
-        img[i].style;
     })
-}
-
-function rotate(){
-    for (let i = 0; i < img.length; i++) {
-        img[i].addEventListener("click", (event) => {
-            img[i].style.transform = "rotate(180deg)";
-            img[i].style.left = img[i].offsetLeft + "px";
-            img[i].style.top = img[i].offsetTop + "px";
-        }
-    )}
 }
